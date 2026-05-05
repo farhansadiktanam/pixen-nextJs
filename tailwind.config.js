bc17a45de0b55afb5afff1d8079d86ed2db2f21b
Farhan Sadik <farhansadik703@gmail.com>
2026-05-05 23:30:25 +0600

    test

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-content: [
-  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  // ⚠️ If you have files outside src/, add them too:
-  // "./pages/**/*.{js,ts,jsx,tsx}",
-  // "./components/**/*.{js,ts,jsx,tsx}",
-],
